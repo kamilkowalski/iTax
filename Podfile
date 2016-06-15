7f3d6f18d68ae51e2c1ec36e99d51d5b1fe9092d
Kamil Kowalski <kowalski.tychy@gmail.com>
2016-06-16 00:17:53 +0200

    Replace code with docs

--- Podfile
+++
@@ -1,3 +0,0 @@
-target 'iTax' do
-  use_frameworks!
-end
--- Pods/Target Support Files/Pods-iTax/Pods-iTax-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <Cocoa/Cocoa.h>
-
-
-FOUNDATION_EXPORT double Pods_iTaxVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_iTaxVersionString[];
-
